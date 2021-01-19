8500d5e905542e53b85a46a207c33cfa25fadcd6
Amit Kumar <amitkecian@gmail.com>
2021-01-19 22:04:12 +0100

    Group the steps in the functions.

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -4,34 +4,47 @@ class GildedRose(var items: Array<Item>) {
 
     fun updateQuality() {
         for (item in items) {
-            if (item.name == ItemConstants.AGED_BRI) {
+            updateQuality(item)
+            updateSellIn(item)
+            updateQualityIfSellInIsNegative(item)
+        }
+    }
+
+    private fun updateSellIn(item: Item) {
+        if (item.name != ItemConstants.SULFURAS) {
+            item.decrementSellIn()
+        }
+    }
+
+    private fun updateQuality(item: Item) {
+        when {
+            item.name == ItemConstants.AGED_BRI -> {
                 item.incrementQuality()
-            } else if (item.name == ItemConstants.BACKSTAGE) {
+            }
+            item.name == ItemConstants.BACKSTAGE -> {
                 item.incrementQuality()
                 updateBackStageQuality(item)
-            } else if (item.name != ItemConstants.SULFURAS) {
+            }
+            item.name != ItemConstants.SULFURAS -> {
                 item.decrementQuality()
-            }
-
-            if (item.name != ItemConstants.SULFURAS) {
-                item.decrementSellIn()
-            }
-
-            if (item.sellIn < 0) {
-                updateQualityIfSellInIsNegative(item)
             }
         }
     }
 
     private fun updateQualityIfSellInIsNegative(item: Item) {
-        if (item.name == ItemConstants.AGED_BRI) {
-            if (item.quality < ItemConstants.MAX_QUALITY) {
+        if (item.sellIn >= 0) {
+            return
+        }
+        when {
+            item.name == ItemConstants.AGED_BRI -> {
                 item.incrementQuality()
             }
-        } else if (item.name == ItemConstants.BACKSTAGE) {
-            item.quality = ItemConstants.MIN_QUALITY
-        } else if (item.name != ItemConstants.SULFURAS) {
-            item.decrementQuality()
+            item.name == ItemConstants.BACKSTAGE -> {
+                item.quality = ItemConstants.MIN_QUALITY
+            }
+            item.name != ItemConstants.SULFURAS -> {
+                item.decrementQuality()
+            }
         }
     }
 
